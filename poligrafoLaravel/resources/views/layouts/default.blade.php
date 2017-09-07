1f3df3ad801d8cbec454df8940fa45876bd1140e
jose Salgado <josetomassalgado91@gmail.com>
2017-09-07 09:36:21 -0400

    solucion error menu

--- poligrafoLaravel/resources/views/layouts/default.blade.php
+++ poligrafoLaravel/resources/views/layouts/default.blade.php
@@ -36,63 +36,6 @@
     <!-- custom poligrafo css -->
     <link href="css/poligrafo/custom.css" rel="stylesheet">
     
-    <!-- jQuery -->
-    <script src="js/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="js/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- FastClick -->
-    <script src="js/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="js/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="js/Chart.js/dist/Chart.min.js"></script>
-    <!-- gauge.js -->
-    <script src="js/gauge.js/dist/gauge.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="js/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <!-- iCheck -->
-    <script src="js/iCheck/icheck.min.js"></script>
-    <!-- Skycons -->
-    <script src="js/skycons/skycons.js"></script>
-    <!-- Flot -->
-    <script src="js/Flot/jquery.flot.js"></script>
-    <script src="js/Flot/jquery.flot.pie.js"></script>
-    <script src="js/Flot/jquery.flot.time.js"></script>
-    <script src="js/Flot/jquery.flot.stack.js"></script>
-    <script src="js/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="js/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="js/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="js/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="js/DateJS/build/date.js"></script>
-    <!-- JQVMap -->
-    <script src="js/jqvmap/dist/jquery.vmap.js"></script>
-    <script src="js/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-    <script src="js/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="js/moment/min/moment.min.js"></script>
-    <script src="js/bootstrap-daterangepicker/daterangepicker.js"></script>
-
-    <!-- Custom Theme Scripts -->
-    <script src="js/build/js/custom.min.js"></script>
-
-    <script src="js/fullcalendar/dist/fullcalendar.js"></script>
-    <script src='js/fullcalendar/dist/lang-all.js'></script>
-    <script src='js/fullcalendar/lib/moment.min.js'></script>
-
-    <!--
-    el que explota
-    <script src='../vendors/fullcalendar/lib/jquery.min.js'></script> -->
-    <script src='js/fullcalendar/fullcalendar.min.js'></script>
-    <script src='js/fullcalendar/locale-all.js'></script>
-
-    <!-- Switchery -->
-    <script src="js/switchery/dist/switchery.min.js"></script>
-    
-    <!--validate jquery-->
-    <script src="js/validate/jquery.validate.js"></script>
-    
 </head>
 <body class="nav-md">
     <div class="container body">
@@ -292,6 +235,59 @@
         <!-- /footer content -->
       </div>
     </div>
+    
+    <!-- jQuery -->
+    <script src="js/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="js/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="js/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="js/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="js/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="js/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="js/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="js/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="js/Flot/jquery.flot.js"></script>
+    <script src="js/Flot/jquery.flot.pie.js"></script>
+    <script src="js/Flot/jquery.flot.time.js"></script>
+    <script src="js/Flot/jquery.flot.stack.js"></script>
+    <script src="js/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="js/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="js/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="js/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="js/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="js/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="js/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="js/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="js/moment/min/moment.min.js"></script>
+    <script src="js/bootstrap-daterangepicker/daterangepicker.js"></script>
 
+    <!-- Custom Theme Scripts -->
+    <script src="js/build/js/custom.min.js"></script>
+
+    <script src="js/fullcalendar/dist/fullcalendar.js"></script>
+    <script src='js/fullcalendar/dist/lang-all.js'></script>
+    <script src='js/fullcalendar/lib/moment.min.js'></script>
+
+    <!--
+    el que explota
+    <script src='../vendors/fullcalendar/lib/jquery.min.js'></script> -->
+    <script src='js/fullcalendar/fullcalendar.min.js'></script>
+    <script src='js/fullcalendar/locale-all.js'></script>
+
+    <!-- Switchery -->
+    <script src="js/switchery/dist/switchery.min.js"></script>
 </body>
 </html>
