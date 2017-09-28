2c719ff6574d300b493a5f92fd5af6aa0f106a06
rafael <velasquezjrvo@gmail.com>
2017-09-28 13:54:34 -0400

    avance pdf edo cuenta

--- poligrafoLaravel/resources/views/pdf/butget.blade.php
+++ poligrafoLaravel/resources/views/pdf/butget.blade.php
@@ -79,24 +79,61 @@
                 </div>
                 <!-- /.col -->
             </div>
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <h5><b>¿Quiénes Somos?</b></h5>
+
+            <div class="row" style="text-align: justify">
+                <div class="col-md-12 col-sm-12 col-xs-12" >
+                    <h5 style="font-size: 14px; font-weight: bold;"><b>¿Quiénes Somos?</b></h5>
+                    <div style="padding-left: 30px;">
                     {{--<ul>--}}
-                        <li>
-                            Somos una empresa con más de  20 años de experiencia en todo el ámbito de la poligrafía, tenemos oficinas en la <b>Ciudad de Panamá, Provincias Centrales y Chiriquí.</b>
-                        </li>
-                        <li>
-                            Realizamos evaluaciones poligráficas con la más alta calidad y profesionalismo, <b>todos nuestros poligrafista son Psicólogos. </b>
-                        </li>
-                        <li>
-                            Somos la única empresa privada en Panamá,  que realiza proceso de control de calidad en todos sus procesos.
-                        </li>
+                        <div>
+                           * Somos una empresa con más de  20 años de experiencia en todo el ámbito de la poligrafía, tenemos oficinas en la <b>Ciudad de Panamá, Provincias Centrales y Chiriquí.</b>
+                        </div>
+                        <div>
+                           * Realizamos evaluaciones poligráficas con la más alta calidad y profesionalismo, <b>todos nuestros poligrafista son Psicólogos. </b>
+                        </div>
+                        <div>
+                           * Somos la única empresa privada en Panamá,  que realiza proceso de control de calidad en todos sus procesos.
+                        </div>
                     {{--</ul>--}}
+                    </div>
                     <br>
                     <p>Contamos con una escuela de poligrafistas la cual está acreditada internacionalmente por la APA (AMERICAN POLYGRAPH ASSOCIATION), nuestro director es ex presidente de la APA y nuestra escuela a entrenado a la unidad de poligrafía de diversas instituciones gubernamentales en diferentes pises de Latinoamérica, desde México, hasta Argentina y en nuestro país, participó en el entrenamiento de las unidades de poligrafía de: La Policía Nacional, el Senan, Senafrond, Dirección de Migración entre otros. <br>
-                        Somos distribuidores exclusivos de equipos poligráficos computarizados Lafayette en Panamá.</p> <br>
+                        Somos distribuidores exclusivos de equipos poligráficos computarizados Lafayette en Panamá.
+                    </p> <br>
+                    <h5 style="font-size: 14px;font-weight: bold;"><b>¿Por qué utilizar el polígrafo?</b></h5>
+                    <p>El Polígrafo es utilizado mundialmente por gobiernos federales y sus diversas instituciones, servicios de inteligencia, instituciones militares, policía, agencias de investigación, así como seguimiento para procesos de libertad condicional. Los gobiernos ponen su confianza en esta herramienta científica, por su gran utilidad en la toma de decisiones. </p>
+                    <p>En el sector privado, es una herramienta que permite conocer elementos de lealtad, honestidad y confiabilidad, en  áreas de investigación criminal, selección y reclutamiento de personal, así como también en las evaluaciones periódicas al personal activo, sobretodo en áreas sensibles.  </p>
+                    <p>Estamos convencidos de que la correcta y puntual evaluación de estos factores tiene un impacto directo en la obtención de las metas de su organización y en el consecuente desarrollo exitoso.</p>
+                    <p>A continuación le presentamos un resumen de nuestros servicios, y nos reiteramos nuevamente a sus órdenes en materia de evaluaciones poligráficas.</p> <br>
 
+                    <h5 style="font-size: 14px; font-weight: bold;"><b>Tipos de Evaluaciones</b></h5>
+                    <h5 style="font-size: 14px; padding-left: 30px;font-weight: bold;"><b>* Pre-empleo</b></h5>
+                        <p style="padding-left: 50px;">En este tipo de evaluaciones se busca prevenir situaciones delicadas al conocer con anticipación elementos de honestidad y confiabilidad del candidato. El objetivo es  prevenir la entrada a su empresa de un delincuente ocupacional potencial. Se revisa y entrega en el reporte:
+                        {{--<ul>--}}
+                            <div style="padding-left:80px;">* Honestidad:
+                                {{--<ul>--}}
+                                    <div style="padding-left:50px;">* En su vida diaria</div>
+                                    <div style="padding-left:50px;">* En su solicitud</div>
+                                {{--</ul>--}}
+                            </div>
+                             <div style="padding-left:80px;">
+                                    <div>* Perfil básico de personalidad</div>
+                                    <div>* Antecedentes Personales</div>
+                                    <div>* Antecedentes laborales</div>
+                                    <div>* Actividades Delictivas</div>
+                                    <div>* Hábitos Personales (Consumo de alcohol y/o drogas)</div>
+                                    <div>* Antecedentes Penales, asociacion delictiva</div>
+                                    <div>* Motivos reales de salida de cada empleo y motivos reales por lo que desea ingresar a la empresa solicitante.</div>
+                             </div>
+                             <h5 style="font-size: 14px; font-weight: bold; padding-left: 30px;"><b>* Evaluaciones Periódicas</b></h5>
+                             <p style="padding-left: 50px;">Es la llamada evaluación especial o específica, en donde se evalúa al individuo de quien se sospecha participación en cualquier actividad ilícita y consta de los siguientes puntos:</p>
+                            <div style="padding-left:80px;">
+                                <div>* Autorización escrita del evaluado</div>
+                                <div>* Análisis médico-psicológico del individuo para determinar la conveniencia de aplicar la evaluación</div>
+                                <div>* Revisión del caso</div>
+                                <div>* Aplicación y análisis de gráficas</div>
+                                <div>* Emisión de un diagnóstico</div>
+                            </div>
                 </div>
             </div>
 
