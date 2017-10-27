8578c5d1c1a058f62d13238c88a15d396641cf76
rafael <velasquezjrvo@gmail.com>
2017-10-27 16:54:38 -0400

    tarea

--- poligrafoLaravel/resources/views/homework/homework.blade.php
+++ poligrafoLaravel/resources/views/homework/homework.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
 <!-- page content -->
-        
+
           <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel tile">
